3aee6747d43a785ec996e1e13d8f06c9990df176
esqabc <1826818052@qq.com>
2019-05-23 16:20:27 +0800

    impror

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,16 @@
+/**
+ * author：esqabc
+ * Time：2019-11-11
+ * Effect：login
+ */
+
+	
+/**
+ * 加载
+ */
+$(function() {
+
+	document.getElementById("login-title").innerHTML="欢迎来到登录页面，我在java以jar包运行的哦";
+	document.getElementById("login-Website").innerHTML='>>>&nbsp;&nbsp;请您多多关照一下我的网站吧 <a href="http://www.esqabc.com/">www.esqabc.com</a>&nbsp;&nbsp;<<<';
+});
+
